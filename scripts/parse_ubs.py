761d0987400831927428246b942875091998a796
Sergey Mechtaev <mechtaev@gmail.com>
2019-08-17 16:41:23 +0100

    extracting relevant ubs data

--- scripts/parse_ubs.py
+++ scripts/parse_ubs.py
@@ -15,6 +15,14 @@ def get_first_2018_index():
       print(i)
       break
 
+interesting_labels = [
+  'Earnings: Average annual (gross)',
+  'General: Domestic Purchasing Power',
+  'Prices: Housing',
+  'Prices: Total expenditure on goods and services',
+  'Prices: Food'
+]
+
 # A - year
 # B - city
 # C - data type
@@ -30,17 +38,22 @@ def print_next_five(base):
                                       ws['E' + str(base + i)].value))
 
 
-# Earnings: Working hours and vacation days (average)
-# list of tuples of size 3:
-ubs_data = []
-for i in range(1,200000):
+# label -> list of tuples of size 3:
+ubs_data = {}
+for label in interesting_labels:
+  ubs_data[label] = []
+
+for label in interesting_labels:
+  for i in range(1,200000):
     if ws['A' + str(i)].value == 2018 and \
-       ws['C' + str(i)].value == 'Earnings: Working hours and vacation days (average)':
-        ubs_data.append((ws['B' + str(i)].value, ws['D' + str(i)].value, ws['E' + str(i)].value))
+       ws['C' + str(i)].value == label and \
+       (ws['D' + str(i)].value == '(USD)' or (label == 'General: Domestic Purchasing Power' and ws['D' + str(i)].value == 'Net (New York = 100)') ):
+        ubs_data[label].append((ws['B' + str(i)].value, ws['D' + str(i)].value, ws['E' + str(i)].value))
 
 with open('ubs_data.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',')
-    spamwriter.writerow(['City', 'Value description', 'Value'])
-    for d in ubs_data:
-        spamwriter.writerow([d[0], d[1], d[2]])
+    spamwriter.writerow(['City', 'Label', 'Value description', 'Value'])
+    for label in interesting_labels:
+      for d in ubs_data[label]:  
+        spamwriter.writerow([d[0], label, d[1], d[2]])
 
